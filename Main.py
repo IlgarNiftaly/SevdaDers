a2a5b5d872b9070d013b75ca0c8f7810d201d0df
IlgarNiftaly <158113662+IlgarNiftaly@users.noreply.github.com>
2024-10-02 16:45:49 +0400

    1-4

---
+++ Main.py
@@ -0,0 +1,66 @@
+# # Nümunə 1
+# hesab = float(input("Hesab: "))
+# hesab = (hesab*115)/100
+# print(f"Hesab: {hesab:.2f}  ", f"Hər bir şəxs ödəməlidir {hesab/2:.2f}")
+
+## Nümunə 2
+#
+# for i in range(3):
+#     reqem = int(input("reqem daxil edin: "))
+#     print(reqem ** 2)
+
+## Nümunə 3
+
+# print("A", end=" ")
+# print("B", end=" ")
+# for i in range(5):
+#     print("C D", end="")
+# print(end=" ")
+# print("E")
+#
+
+
+
+# Nümunə 4
+
+print("A", end=" ")
+print("B", end=" ")
+for i in range(5):
+    print("C", end="")
+print(end=" ")
+for i in range(5):
+    print("D", end="")
+print(end=" ")
+print("E")
+
+
+
+## Nümunə 5
+
+
+## Nümunə 6
+
+
+## Nümunə 7
+
+
+## Nümunə 8
+
+
+## Nümunə 9
+
+
+## Nümunə 10
+
+
+## Nümunə 11
+
+
+## Nümunə 12
+
+
+
+
+
+
+
